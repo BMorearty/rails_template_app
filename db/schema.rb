916e06122a448804783dfb064351e9091616ea2e
Brian Morearty <brian@morearty.org>
2014-09-26 20:44:21 -0700

    Updates for Sorcery.

--- db/schema.rb
+++ db/schema.rb
@@ -9,17 +9,20 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627155446) do
+ActiveRecord::Schema.define(version: 20120627155446) do
 
-  create_table "users", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email",                                                                     :null => false
+    t.string   "email",                                                                  null: false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",                                                                :null => false
-    t.datetime "updated_at",                                                                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
@@ -28,18 +31,19 @@ ActiveRecord::Schema.define(:version => 20120627155446) do
     t.datetime "reset_password_email_sent_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.integer  "failed_logins_count",             :default => 0
+    t.integer  "failed_logins_count",             default: 0
     t.datetime "lock_expires_at"
     t.datetime "last_login_at"
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
+    t.string   "last_login_from_ip_address"
     t.string   "role"
-    t.string   "time_zone",                       :default => "Pacific Time (US & Canada)", :null => false
+    t.string   "time_zone",                       default: "Pacific Time (US & Canada)", null: false
   end
 
-  add_index "users", ["activation_token"], :name => "index_users_on_activation_token"
-  add_index "users", ["last_logout_at", "last_activity_at"], :name => "index_users_on_last_logout_at_and_last_activity_at"
-  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
+  add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
+  add_index "users", ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
 end
