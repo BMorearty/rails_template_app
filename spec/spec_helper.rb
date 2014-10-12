0b59ef755f0cd465ab33e7b74a2be8d13a0f1071
Brian Morearty <brian@morearty.org>
2014-10-11 20:31:18 -0700

    Use truncation when running JavaScript specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,17 +24,27 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   # Adds login_user(user=@user)) and logout_user helpers.  Only for use in functional tests.
   config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
-  config.include Sorcery::TestHelpers::Rails::Integration, type: :feature
+  config.include Sorcery::TestHelpers::Rails::Integration, type: :request
   config.include RequestSpecHelpers, type: :request
   config.include CapybaraExtensions
   config.include Capybara::DSL, type: :request
   config.include ShowMeTheCookies, :type => :request
   config.include BM
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.before :each do
+    if Capybara.current_driver == :rack_test
+      DatabaseCleaner.strategy = :transaction
+    else
+      DatabaseCleaner.strategy = :truncation
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
